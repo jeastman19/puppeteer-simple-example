f7ad74103e0563ae9e656873ddfb1cbfb4d58b38
Jorge Eastman <jeastman19@gmail.com.com>
2019-07-19 01:45:59 +0000

    Se agrega soporte para .env

--- example.js
+++ example.js
@@ -1,19 +1,19 @@
+const dotenv = require('dotenv').config();
 const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({
         args: ['--no-sandbox'],
-        headless: false,
-        executablePath: '/usr/bin/google-chrome'
+        headless: true
     });
     const page = await browser.newPage();
-    await page.goto('https://formulario08digital.com');
+    await page.goto(process.env.PAGE_URL);
     await page.setViewport({
         width: 1366,
         height: 768
     })
     await page.screenshot({
-        path: 'example.png'
+        path: 'capture.png'
     });
 
     await browser.close();
